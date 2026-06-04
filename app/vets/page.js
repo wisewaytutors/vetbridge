cc17769485bc4a2cae6ce990f72bb9cde1efeb4b
Gebremeskel Abreham <gebremeskel612@gmail.com>
2026-06-04 14:22:36 +0300

    Adjust vets page search bar size to medium

--- app/vets/page.js
+++ app/vets/page.js
@@ -65,7 +65,7 @@ export default function VetsPage() {
 
         {/* Search */}
         <form onSubmit={handleSearch} className="mb-6">
-          <div className="relative">
+          <div className="relative max-w-2xl mx-auto">
             <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
               type="text"
